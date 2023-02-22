7729dac1647f2189574c82b1d065f8b839fed4c5
SamuelMartosCarmet <124789327+SamuelMartosCarmet@users.noreply.github.com>
2023-02-22 13:54:45 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,35 @@
 input.onButtonPressed(Button.A, function () {
     if (pulsado == false) {
-        let numero_de_imagen = ""
         pulsado = true
         if (numero_de_imagen == "0" || (numero_de_imagen == "3" || numero_de_imagen == "7")) {
             puntuacion_1 += 1
-            basic.showString("" + (puntuacion_1))
+            if (puntuacion_1 == 10) {
+                basic.showString("A: has ganado")
+                juego_detenido = true
+            }
+        } else if (numero_de_imagen == "6") {
+            basic.showString("A: has perdido")
+            basic.pause(5000)
+        } else if (puntuacion_1 > 0) {
+            puntuacion_1 += -1
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (pulsado == false) {
+        pulsado = true
+        if (numero_de_imagen == "1" || (numero_de_imagen == "4" || numero_de_imagen == "8")) {
+            puntuacion_b += 1
+            if (puntuacion_b == 10) {
+                basic.showString("A: has ganado")
+                juego_detenido = true
+            }
         } else if (numero_de_imagen == "6") {
             basic.showString("A: has perdido")
             basic.pause(5000)
         } else {
-            if (puntuacion_1 > 0) {
-                puntuacion_1 += -1
-                basic.showString("" + (puntuacion_1))
+            if (puntuacion_b > 0) {
+                puntuacion_b += -1
             }
         }
     }
@@ -21,12 +39,13 @@ function Cojer_elemento_de_la_lista () {
     lista[icono].showImage(0)
     basic.pause(200)
 }
+let juego_detenido = false
+let numero_de_imagen = ""
 let pulsado = false
-let lista: Image[] = []
+let puntuacion_b = 0
 let icono = 0
 let puntuacion_1 = 0
-puntuacion_1 = 0
-icono = 0
+let lista: Image[] = []
 lista = [
 images.createImage(`
     . # # # .
@@ -99,6 +118,9 @@ images.createImage(`
     # . . . #
     `)
 ]
+puntuacion_1 = 0
+icono = 0
+puntuacion_b = 0
 basic.forever(function () {
     Cojer_elemento_de_la_lista()
 })
